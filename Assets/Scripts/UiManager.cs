f8cf3c745514a04c71fef41e5829bd91840ac0e2
odarennbi <ambrazhey.maxim@gmail.com>
2019-03-05 21:53:47 +0300

    - doTween plugin and game screen (base logic) added

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    private static UiManager instance;
+
+
+    public static UiManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
