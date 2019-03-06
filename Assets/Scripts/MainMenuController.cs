532d26bffe2fdaa81a54b2e48f1df89b6d9a0892
ZiGNicK <baulkynut@gmail.com>
2019-03-06 14:26:43 +0300

    TMpro + MainMenu + backToMainMenu button in mainScene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
