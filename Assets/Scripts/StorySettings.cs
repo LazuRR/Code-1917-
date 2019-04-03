95861d5a73cac17be4c9aa7d03e82ef6d2857939
odarennbi <ambrazhey.maxim@gmail.com>
2019-04-03 15:56:46 +0300

    - add add field for message

--- Assets/Scripts/StorySettings.cs
+++ Assets/Scripts/StorySettings.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class StorySettings : ScriptableObject
 {
     public string message;
+    public bool isMessageOnly;
 
     public AnswerInfo positiveAnswerInfo;
     public AnswerInfo negativeAnswerInfo;
